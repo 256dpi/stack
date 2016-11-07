427f403ac5b190c88ef6047df121cb081707eb6b
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-11-07 20:22:23 +0100

    first commit

---
+++ ctrl.go
@@ -0,0 +1,44 @@
+// Package ctrl provides a very small and clean API for using the built-in panic
+// and recover functions to abort and resume execution of a goroutine.
+package ctrl
+
+import "runtime/debug"
+
+// Cause wraps errors handled by Abort and Resume.
+type Cause struct {
+	Err error
+}
+
+// Abort will abort even if the supplied error is nil.
+func Abort(err error) {
+	panic(&Cause{err})
+}
+
+// AbortIf will only abort if the supplied error is present.
+func AbortIf(err error) {
+	if err != nil {
+		Abort(err)
+	}
+}
+
+// Resume will try to recover an earlier call to Abort and call fn if an error
+// has been recovered. It will not recover direct calls to the built-in panic
+// function.
+//
+// Note: If the built-in panic function has been called with nil, a call to
+// Resume will discard that panic and continue execution.
+func Resume(fn func(error)) {
+	val := recover()
+	if cause, ok := val.(*Cause); ok {
+		fn(cause.Err)
+		return
+	} else if val != nil {
+		panic(val)
+	}
+}
+
+// Stack returns a formatted stack trace of the original call to Abort during
+// a call to Resume.
+func Stack() []byte {
+	return debug.Stack()
+}
---
+++ ctrl_test.go
@@ -0,0 +1,73 @@
+package ctrl
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var errAbortTest = errors.New("foo")
+
+func TestAbortIfResume(t *testing.T) {
+	var test error
+
+	func() {
+		defer Resume(func(err error) {
+			test = err
+		})
+
+		AbortIf(errAbortTest)
+	}()
+
+	assert.Equal(t, errAbortTest, test)
+}
+
+func TestPanic(t *testing.T) {
+	defer func() {
+		recover()
+	}()
+
+	var test error
+
+	func() {
+		defer Resume(func(err error) {
+			test = err
+		})
+
+		panic(errAbortTest)
+	}()
+
+	assert.Nil(t, test)
+}
+
+func TestAbortIfNil(t *testing.T) {
+	var test error
+
+	func() {
+		defer Resume(func(err error) {
+			test = err
+		})
+
+		AbortIf(nil)
+	}()
+
+	assert.Nil(t, test)
+}
+
+func TestStack(t *testing.T) {
+	var test error
+	var stack string
+
+	func() {
+		defer Resume(func(err error) {
+			test = err
+			stack = string(Stack())
+		})
+
+		Abort(errAbortTest)
+	}()
+
+	assert.Equal(t, errAbortTest, test)
+	assert.Contains(t, stack, "ctrl.Abort")
+}
